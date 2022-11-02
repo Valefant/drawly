5c52c2b639a2e8f3f48a5cfa267616ac7da94938
Valefant <19901781+Valefant@users.noreply.github.com>
2022-11-02 10:59:12 +0100

    Add viewport and description meta tags to root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,8 @@ export default function RootLayout({
     <html lang="en" className={font.className}>
       <head>
         <title>{title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={pkg.description} />
       </head>
       <body>{children}</body>
     </html>
