6ab3d36ca93eba2f5b4a41b3793004fb0ebc758c
Valefant <19901781+Valefant@users.noreply.github.com>
2022-11-06 15:13:01 +0100

    Add image filters
    closes #3

--- app/draw/[slug]/frame.tsx
+++ app/draw/[slug]/frame.tsx
@@ -12,6 +12,13 @@ import useWebAnimations from '@wellyshen/use-web-animations';
 import Attribution from '../../../components/attribution';
 import { useRef, useState } from 'react';
 import { playfulButton } from '../../../components/design';
+import {
+  getImageData,
+  grayscale,
+  printCanvas,
+  roberts,
+  thresholding,
+} from 'lena-ts';
 
 function useRotation() {
   const [rotation, setRotation] = useState(0);
@@ -19,6 +26,41 @@ function useRotation() {
     setRotation((prev) => prev + 90);
   };
   return { rotation, rotate };
+}
+
+const filters = ['gray', 'edge', 'shadows'] as const;
+type Filter = typeof filters[number];
+const filterMapping: { [key in Filter]: (pixels: ImageData) => ImageData } = {
+  gray: grayscale,
+  edge: roberts,
+  shadows: thresholding,
+};
+
+interface FilterSelectionProps {
+  activeFilter: Filter | null;
+  onFilterChange: (filter: Filter | null) => void;
+}
+
+function FilterSelection({
+  activeFilter,
+  onFilterChange,
+}: FilterSelectionProps) {
+  return (
+    <div className="flex space-x-4">
+      {filters.map((filter, i) => (
+        <button
+          title={`${filter} (${i + 1})`}
+          className={playfulButton({
+            intent: activeFilter === filter ? 'active' : 'primary',
+          })}
+          key={filter}
+          onClick={() => onFilterChange(filter)}
+        >
+          {filter}
+        </button>
+      ))}
+    </div>
+  );
 }
 
 export function Frame({
@@ -29,14 +71,16 @@ export function Frame({
   photos: Photo[];
 }) {
   const audioRef = useRef<HTMLAudioElement>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
   const [currentStep, { goToNextStep }] = useStep(photos.length);
+  const [activeFilter, setActiveFilter] = useState<Filter | null>(null);
   const { value: isPlaying, toggle: toggleTimer } = useBoolean(true);
   const { value: flipped, toggle: toggleFlip } = useBoolean(false);
   const { rotation, rotate } = useRotation();
   // step starts by 1
   const index = currentStep - 1;
   const photo = photos[index];
-  const buttonClasses = playfulButton();
 
   const { ref, getAnimation } = useWebAnimations<HTMLDivElement>({
     keyframes: { opacity: [1, 0] },
@@ -47,7 +91,39 @@ export function Frame({
 
   const nextImage = () => {
     goToNextStep();
+    // todo: active filter should be applied to the next image as well
+    setActiveFilter(null);
     getAnimation()?.play();
+  };
+
+  const changeFilterHandler = (filter: Filter | null) => {
+    if (imgRef.current === null || canvasRef.current === null) {
+      return;
+    }
+
+    if (filter === null) {
+      setActiveFilter(null);
+      return;
+    }
+
+    const imageData = getImageData(imgRef.current);
+    const filterAppliedImageData = filterMapping[filter](imageData);
+    printCanvas(canvasRef.current, filterAppliedImageData);
+
+    if (filter === activeFilter) {
+      setActiveFilter(null);
+    } else {
+      setActiveFilter(filter);
+    }
+  };
+
+  const synchronizeCanvas = () => {
+    if (imgRef.current === null || canvasRef.current === null) {
+      return;
+    }
+
+    canvasRef.current.width = imgRef.current.width;
+    canvasRef.current.height = imgRef.current.height;
   };
 
   useEventListener('keydown', (e) => {
@@ -73,6 +149,10 @@ export function Frame({
     if (e.key === 'r') {
       rotate();
     }
+
+    if (e.key === '1' || e.key === '2' || e.key === '3') {
+      changeFilterHandler(filters[Number(e.key) - 1]);
+    }
   });
 
   return (
@@ -85,7 +165,18 @@ export function Frame({
         }}
       >
         <div className="absolute inset-0 z-20 bg-white opacity-0" ref={ref} />
-        <img alt={photo.alt as string} src={photo.src.large} />
+        <img
+          crossOrigin="anonymous"
+          ref={imgRef}
+          alt={photo.alt as string}
+          src={photo.src.large}
+          onLoad={synchronizeCanvas}
+          className={activeFilter ? 'hidden' : ''}
+        />
+        <canvas
+          ref={canvasRef}
+          className={activeFilter ? '' : 'hidden'}
+        ></canvas>
       </picture>
       <Attribution
         className="flex opacity-50"
@@ -99,7 +190,7 @@ export function Frame({
       <div className="md:scale-100 sm:scale-75 md:space-x-8 flex items-center space-x-6 scale-50">
         <div className="flex flex-col">
           <button
-            className={buttonClasses}
+            className={playfulButton()}
             onClick={() => toggleTimer()}
             title={`${isPlaying ? 'Pause' : 'Play'} (p)`}
           >
@@ -108,7 +199,7 @@ export function Frame({
         </div>
         <audio ref={audioRef} src="/sound.wav"></audio>
         <CountdownCircleTimer
-          duration={duration * 60}
+          duration={duration * 10}
           isPlaying={isPlaying}
           key={currentStep}
           colors={'#000000'}
@@ -124,27 +215,32 @@ export function Frame({
           )}
         </CountdownCircleTimer>
         <div className="flex flex-col space-y-2">
+          <FilterSelection
+            activeFilter={activeFilter}
+            onFilterChange={changeFilterHandler}
+          />
           <div className="flex justify-center space-x-2">
             <button
-              className={buttonClasses}
+              className={playfulButton()}
               onClick={() => rotate()}
               title="Rotate (r)"
             >
               rotate
             </button>
             <button
-              className={buttonClasses}
+              className={playfulButton({
+                intent: flipped ? 'active' : 'primary',
+              })}
               onClick={() => toggleFlip()}
               title="Flip (f)"
             >
-              {flipped && <div className="w-2 h-2 bg-blue-600 rounded-full" />}{' '}
               flip
             </button>
           </div>
         </div>
         <div className="space-y-2">
           <button
-            className={`flex ${buttonClasses}`}
+            className={`flex ${playfulButton()}`}
             onClick={() => {
               nextImage();
             }}
