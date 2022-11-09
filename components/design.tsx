42a267f22caaf91eb9a5eeb0bbc194c8dc651e80
Valefant <19901781+Valefant@users.noreply.github.com>
2022-11-09 07:50:27 +0100

    Minor styling adjustment

--- components/design.tsx
+++ components/design.tsx
@@ -8,7 +8,7 @@ export const playfulButton = cva(
         primary:
           'bg-transparent text-black hover:bg-slate-700 hover:text-white dark:text-white dark:hover:bg-slate-300 dark:hover:text-black',
         active: 'bg-black text-white dark:bg-white dark:text-black',
-        disabled: 'border-transparent opacity-50',
+        disabled: 'opacity-25',
       },
       size: {
         small: 'text-sm py-1 px-2',
