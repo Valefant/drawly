0d84bc479fe476919f17d948e87b22d5c57cd3f7
Valefant <19901781+Valefant@users.noreply.github.com>
2022-11-18 09:54:29 +0100

    Differentiate loading states

--- app/draw/[category]/loading.tsx
+++ app/draw/[category]/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="dark:bg-neutral-800 dark:text-white flex items-center justify-center h-screen text-3xl">
-      Loading application...
+      Loading images...
     </div>
   );
 }
